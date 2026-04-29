fb2d6d7351303f3e1af01e047c70780fd502be6f
Jagannath Das <129247973+jagannath-das@users.noreply.github.com>
2026-04-29 23:25:38 +0530

    Change ID generation strategy to IDENTITY
    it is giving userdb error

--- src/main/java/com/proj/model/User.java
+++ src/main/java/com/proj/model/User.java
@@ -32,7 +32,7 @@ import lombok.NoArgsConstructor;
 public class User 
 {
 @Id
-@GeneratedValue (strategy=GenerationType.AUTO)
+@GeneratedValue(strategy = GenerationType.IDENTITY)
 private Long id;
 private String fullname;
 @NotNull
